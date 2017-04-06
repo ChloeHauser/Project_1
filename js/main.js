efa00eb8b8c0a483ab7fa928825485efc9084119
ChloeHauser <Chloe_Hauser@usd368.org>
2017-04-06 14:37:04 -0500

    final-turn-in

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 console.log('works');
+@-webkit-keyframes slideIn {
+    from {bottom: -300px; opacity: 0} 
+    to {bottom: 0; opacity: 1}
+}
+
+@keyframes slideIn {
+    from {bottom: -300px; opacity: 0}
+    to {bottom: 0; opacity: 1}
+}
+
+@-webkit-keyframes fadeIn {
+    from {opacity: 0} 
+    to {opacity: 1}
+}
+
+@keyframes fadeIn {
+    from {opacity: 0} 
+    to {opacity: 1}
+}
